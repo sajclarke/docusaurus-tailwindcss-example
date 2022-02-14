aaa239ee50196b6d6c76c519bd81f964a96904f0
sajclarke <shannonajclarke@gmail.com>
2022-02-14 05:14:57 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className="bg-blue-500">
+      <div className="container mx-auto text-center py-24">
+        <h1 className="text-4xl font-bold text-white">{siteConfig.title}</h1>
+        <p className="text-xl py-6 text-white">{siteConfig.tagline}</p>
+
+        <div className="py-10">
+          <Link
+            className="bg-white rounded-md text-gray-500 px-4 py-2"
+            to="/docs/intro"
+          >
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
